a15180c5f3eed8ef5a3133d9605ef73c3e8b60c6
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-06 20:54:04 -0500

    bridesmaids created

---
+++ app/assets/javascripts/components/Bridesmaids.js
@@ -0,0 +1,53 @@
+"use strict";
+
+Vue.component('bridesmaids', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: `<div class="wedding-party">
+              <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Matron of Honour</h5>
+                  <img class="avatar" src="https://pp.userapi.com/6Kf5KNYniYRWlI5HlKBpDIgWe1TIHfYivQgnzg/-jXA4eIpy4E.jpg?ava=1"/>
+                  <p class="card-text">Kim Frumkin</p>
+                </div>
+              </div>
+              <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Bridesmaid</h5>
+              <img class="avatar" src="https://pp.userapi.com/6Kf5KNYniYRWlI5HlKBpDIgWe1TIHfYivQgnzg/-jXA4eIpy4E.jpg?ava=1"/>
+              <p class="card-text">Blaire</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Bridesmaid</h5>
+              <img class="avatar" src="https://pp.userapi.com/6Kf5KNYniYRWlI5HlKBpDIgWe1TIHfYivQgnzg/-jXA4eIpy4E.jpg?ava=1"/>
+              <p class="card-text">Erica</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Bridesmaid</h5>
+              <img class="avatar" src="https://pp.userapi.com/6Kf5KNYniYRWlI5HlKBpDIgWe1TIHfYivQgnzg/-jXA4eIpy4E.jpg?ava=1"/>
+              <p class="card-text">Lizzie</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Bridesmaid</h5>
+              <img class="avatar" src="https://pp.userapi.com/6Kf5KNYniYRWlI5HlKBpDIgWe1TIHfYivQgnzg/-jXA4eIpy4E.jpg?ava=1"/>
+              <p class="card-text">Mary</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Bridesmaid</h5>
+              <img class="avatar" src="https://pp.userapi.com/6Kf5KNYniYRWlI5HlKBpDIgWe1TIHfYivQgnzg/-jXA4eIpy4E.jpg?ava=1"/>
+              <p class="card-text">Samantha</p>
+              </div>
+            </div>
+            <div>`
+});
