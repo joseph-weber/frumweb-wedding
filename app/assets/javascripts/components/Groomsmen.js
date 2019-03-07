d2e9d13ba2f7f2b55caeeb53d4110c8f18e013c8
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-06 20:54:20 -0500

    groomsmen component added

---
+++ app/assets/javascripts/components/Groomsmen.js
@@ -0,0 +1,53 @@
+"use strict";
+
+Vue.component('groomsmen', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: `<div class="wedding-party">
+              <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Best Man</h5>
+                  <img class="avatar" src="http://mousebreath.com/wp-content/uploads/2011/11/Puss-in-boots-thumb.jpg"/>
+                  <p class="card-text">David Weber</p>
+                </div>
+              </div>
+              <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Groomsman</h5>
+              <img class="avatar" src="http://mousebreath.com/wp-content/uploads/2011/11/Puss-in-boots-thumb.jpg"/>
+              <p class="card-text">Rob</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Groomsman</h5>
+              <img class="avatar" src="http://mousebreath.com/wp-content/uploads/2011/11/Puss-in-boots-thumb.jpg"/>
+              <p class="card-text">John</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Groomsman</h5>
+              <img class="avatar" src="http://mousebreath.com/wp-content/uploads/2011/11/Puss-in-boots-thumb.jpg"/>
+              <p class="card-text">Phil</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Groomsman</h5>
+              <img class="avatar" src="http://mousebreath.com/wp-content/uploads/2011/11/Puss-in-boots-thumb.jpg"/>
+              <p class="card-text">Zubair</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Groomsman</h5>
+              <img class="avatar" src="http://mousebreath.com/wp-content/uploads/2011/11/Puss-in-boots-thumb.jpg"/>
+              <p class="card-text">Zach</p>
+              </div>
+            </div>
+            <div>`
+});
