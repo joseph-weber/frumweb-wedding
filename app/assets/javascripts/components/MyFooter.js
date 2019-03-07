f0f961448c7b33e617a83bc653afebce6ed03f88
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-06 23:13:03 -0500

    footer created

---
+++ app/assets/javascripts/components/MyFooter.js
@@ -0,0 +1,8 @@
+"use strict";
+
+Vue.component('my-footer', {
+  template: `<footer id="my-footer">
+              <div><p>Created by Joseph Weber</p></div>
+              <div><p>A Labor of Love</p></div>
+            </footer>`,
+});
