674500b5562a5d3507d03255dc84f562d3a30feb
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-07 18:51:55 -0500

    schedule.js created

---
+++ app/assets/javascripts/components/Schedule.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Vue.component('schedule', {
+  template: `<div id="schedule-div">
+              <img id="venue-pic" src="/assets/wedding_invite.jpg" />
+              <h1 class="pac">Friday January 17</h1>
+              <h3 class="pac">Rehearsal Dinner</h3>
+              <p class="pac">6 pm</p>
+              <p class="pac">Where: Rococo Steakhouse</p>
+              <p class="pac">655 2nd Ave S. St. Petersburg, FL</p>
+              <p class="pac">Who: Wedding Party, direct family & aunts and uncles</p>
+              <h3 class="pac">Out of Town Guest Reception</h3>
+              <p class="pac">8 pm</p>
+              <p class="pac">Where: Intermezzo Coffee and Cocktail Bar</p>
+              <p class="pac">1111 Central Ave, St. Petersburg, FL</p>
+              <p class="pac">Who: All Guests</p>
+              <h1 class="pac">Saturday January 18</h1>
+              <h3 class="pac">Wedding Ceremony</h3>
+              <p class="pac">6:30 pm</p>
+              <p class="pac">Where: The James Museum</p>
+              <p class="pac">150 Central Ave, St. Petersburg, FL </p>
+              <p class="pac">Who: All Guests</p>
+            </div>`,
+});
