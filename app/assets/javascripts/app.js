835fdcf8f0da5e2bc2498640a8a67ab99874584d
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-06 23:09:42 -0500

    app js now includes all new components

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,14 +1,33 @@
 "use strict";
 
+var bus = new Vue();
+
+
+// put in created
+// bus.$on('changeFC', (data)=>{
+//  this.$data.FC = data;
+//})
+
+// put in functions for onclicks
+// bus.$emit('changeFC, something from result, something from result)
+
+
+//
+
+
 $(function () { if ($('#app').length) {
 
   new Vue({
     el: '#app',
     data: {
-      hello: 'hello world'
+      hello: 'hello world',
+      page: 'home'
     },
     created: function() {
       this.getMessage();
+      bus.$on('changePage', (data)=>{
+          this.$data.page = data;
+        })
     },
     methods: {
       getMessage: function() {
@@ -32,10 +51,15 @@ $(function () { if ($('#app').length) {
     template: `<div>
                 <my-header></my-header>
                 <nav-bar></nav-bar>
+                <img v-show="page == 'home'" id="the-couple" src="/assets/the_couple.jpg"/>
+                <p v-show="page == 'home'" id="lorem">Tempor est ad veniam officia adipisicing cupidatat. Officia occaecat aute excepteur pariatur eiusmod sint culpa ullamco commodo exercitation est ad id anim. Consectetur aute ea esse et non. Esse id eu quis exercitation dolor do eiusmod. Non ipsum sunt pariatur cillum eiusmod Lorem. Voluptate elit do nisi sit deserunt Lorem elit qui cupidatat.</p>
+                <destination v-show="page == 'destination'"></destination>
                 <div id="wedding-party">
-                  <groomsmen></groomsmen>
-                  <bridesmaids></bridesmaids>
+                  <groomsmen v-show="page == 'home'"></groomsmen>
+                  <bridesmaids v-show="page == 'home'"></bridesmaids>
                 </div>
+                <images-carousel v-show="page == 'photos'"></images-carousel>
+                <my-footer></my-footer>
               </div>`
   })
 }});
