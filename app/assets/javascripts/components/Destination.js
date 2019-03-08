f741419420216002bce5cbdaaf6fc44b619f2831
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-07 21:38:14 -0500

    all locations added to destination

--- app/assets/javascripts/components/Destination.js
+++ app/assets/javascripts/components/Destination.js
@@ -4,8 +4,6 @@ Vue.component('destination', {
   template: `<div id="destination-div">
               <h1>St. Petersburg, FL</h1>
               <img id="destination-pic" src="./assets/st_pete.jpg"/>
-              <h3>Venue: The James Museum</h3>
-              <img id="venue-pic" src="https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzL2RmYWE1ZTc1LWIxYTItNDZmOS05ODdkLTIyZjFkMDQyNDgzOThmN2VkMjA1YzM0ODYzOGIyN18wMDQuSlBHIl0sWyJwIiwidGh1bWIiLCJ4MzkwPiJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA4MSAtYXV0by1vcmllbnQiXV0/004.JPG" />
-              <h4>150 Central Ave, St. Petersburg, FL 33701</h4>
+              <activities />
             </div>`,
 });
