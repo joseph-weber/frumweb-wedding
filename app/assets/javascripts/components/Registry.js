fa579faed75e6a2eaee0f2320444ed7a1065c30b
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-07 21:40:25 -0500

    added registry component

---
+++ app/assets/javascripts/components/Registry.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Vue.component('registry', {
+  template: `<div id="registry-div">
+              <h1>Registry</h1>
+              <h2>Bloomingdales</h2>
+              <a href="">Bloomingdales</a>
+              <h2>Second Place</h2>
+              <a href="">Second Place</a>
+              <h2>Third Place</h2>
+              <a href="">Third Place</a>
+            </div>`,
+});
