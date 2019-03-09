98c6d01412ce1d293ec2afc84ba5b84ac977252b
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-09 12:49:20 -0500

    images carousel is now vueified

--- app/assets/javascripts/components/Registry.js
+++ app/assets/javascripts/components/Registry.js
@@ -1,13 +1,34 @@
 "use strict";
 
 Vue.component('registry', {
+  data: function () {
+    return {
+      items: [
+        {
+          logo: './assets/bloomingdales.svg',
+          link: '#',
+          name: 'Bloomingdales'
+        },
+        {
+          logo: './assets/container.jpg',
+          link: '#',
+          name: 'Container Store'
+        },
+        {
+          logo: './assets/crate.png',
+          link: '#',
+          name: 'Crate and Barrel'
+        }
+      ]
+    }
+  },
   template: `<div id="registry-div">
-              <h1>Registry</h1>
-              <h2>Bloomingdales</h2>
-              <a href="">Bloomingdales</a>
-              <h2>Second Place</h2>
-              <a href="">Second Place</a>
-              <h2>Third Place</h2>
-              <a href="">Third Place</a>
+              <h1 class="title">Registry</h1>
+              <div v-for="item in items" class="large-card card w-60">
+                <div class="registry">
+                  <img class="registry-img" :src="item.logo" />
+                  <a class="registry-link" :href="item.link">{{item.name}}</a>
+                </div>
+              </div>
             </div>`,
 });
