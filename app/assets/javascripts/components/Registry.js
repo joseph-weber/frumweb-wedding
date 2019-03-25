6501945de67da3709c46b9d788d5dd26c1cc1f75
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-24 22:01:16 -0400

    Registry links updated

--- app/assets/javascripts/components/Registry.js
+++ app/assets/javascripts/components/Registry.js
@@ -6,18 +6,24 @@ Vue.component('registry', {
       items: [
         {
           logo: './assets/bloomingdales.svg',
-          link: '#',
+          link: 'https://www.bloomingdales.com/registry/wedding/guest/?perfectProxy=true&registryId=2400758&cm_sp=BWEDD_Find_Registry-_-View_GVR-_-n',
           name: 'Bloomingdales'
         },
         {
           logo: './assets/container.jpg',
-          link: '#',
+          link: 'https://www.bloomingdales.com/registry/wedding/guest/?perfectProxy=true&registryId=2400758&cm_sp=BWEDD_Find_Registry-_-View_GVR-_-n',
           name: 'Container Store'
         },
         {
           logo: './assets/crate.png',
-          link: '#',
+          link: 'https://www.bloomingdales.com/registry/wedding/guest/?perfectProxy=true&registryId=2400758&cm_sp=BWEDD_Find_Registry-_-View_GVR-_-n',
           name: 'Crate and Barrel'
+        },
+        {
+          logo: './assets/casa.png',
+          link: 'https://www.bloomingdales.com/registry/wedding/guest/?perfectProxy=true&registryId=2400758&cm_sp=BWEDD_Find_Registry-_-View_GVR-_-n',
+          name: 'Casa',
+          info: 'Please consider contributing to our donation to Casa St. Pete. Together we can help end domestic abuse while empowering survivors.'
         }
       ]
     }
@@ -27,6 +33,7 @@ Vue.component('registry', {
               <div v-for="item in items" class="large-card card w-60">
                 <div class="registry">
                   <img class="registry-img" :src="item.logo" />
+                  <p>{{item.info}}</p>
                   <a class="registry-link" :href="item.link">{{item.name}}</a>
                 </div>
               </div>
