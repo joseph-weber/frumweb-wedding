6937fb6447618320f82198a7754bab4390993f09
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-24 22:00:34 -0400

    bootstrap carousel updated

--- app/assets/javascripts/components/ImagesCarousel.js
+++ app/assets/javascripts/components/ImagesCarousel.js
@@ -10,19 +10,24 @@ Vue.component('images-carousel', {
         './assets/the_couple.jpg',
         './assets/toledo.jpg',
         './assets/mahaffey.jpg',
-        './assets/boys.jpg'
+        './assets/boys.jpg',
+        './assets/safety.jpg'
       ]
     }
   },
-  template: `<div id="carousel">
+  template: `<div id="carousel-div">
               <h1 class="title">Images</h1>
               <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img class="d-block w-100" src="./assets/cien.jpg" alt="First slide">
+                    <div class="around-image">
+                    <img class="d-block" src="./assets/cien.jpg" alt="First slide">
+                    </div>
                   </div>
                   <div v-for="(item, index) in items" class="carousel-item">
-                    <img class="d-block w-100" :src="item" alt="Second slide">
+                    <div class="around-image">
+                    <img class="d-block" :src="item" alt="Second slide">
+                    </div>
                   </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
