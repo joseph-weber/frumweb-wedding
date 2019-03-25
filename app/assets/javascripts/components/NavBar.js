e130ad34b667921bff047ecc2ceed81d4d40a425
Joseph Weber <joseph.theodore.weber@gmail.com>
2019-03-24 22:01:00 -0400

    nav bar updated for media query

--- app/assets/javascripts/components/NavBar.js
+++ app/assets/javascripts/components/NavBar.js
@@ -3,29 +3,41 @@
 Vue.component('nav-bar', {
   data: function () {
     return {
-      count: 0
+      dropDown: false
+    }
+  },
+  created: function() {
+    if ($(window).width() > 700) {
+      this.$data.dropDown = false;
     }
   },
   methods: {
     pageChange: function (page) {
-      bus.$emit('changePage', page)
+      bus.$emit('changePage', page);
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
     },
-    registry: function () {
-      console.log('registry');
-      location.replace('http://bloomingdales.com');
-    }
+    dropItDown: function () {
+      bus.$emit('hamburgerClick');
+      this.$data.dropDown = !this.$data.dropDown;
+    },
   },
-  template: `<div id="navigation-bar" class="navbar navbar-default navbar-top topnav" role="navigation">
-              <div><p v-on:click="pageChange('home')" class="nav-p">Home</p></div>
-              <p class="nav-p">|</p>
-              <div><p v-on:click="pageChange('schedule')" class="nav-p">Schedule</p></div>
-              <p class="nav-p">|</p>
-              <div><p v-on:click="pageChange('photos')" class="nav-p">Photos</p></div>
-              <p class="nav-p">|</p>
-              <div><p v-on:click="pageChange('destination')" class="nav-p">Destination</p></div>
-              <p class="nav-p">|</p>
-              <div><p v-on:click="pageChange('registry')"  class="nav-p">Registry</p></div>
-              <p class="nav-p">|</p>
-              <div><p v-on:click="pageChange('accomodations')"  class="nav-p">Accomodations</p></div>
-            </div>`,
+  template: `<nav>
+              <div id="navigation-bar">
+                <div class="nav-div"><p v-on:click="pageChange('home')" class="nav-p">Home</p></div>
+                <p class="nav-p">|</p>
+                <div class="nav-div"><p v-on:click="pageChange('schedule')" class="nav-p">Schedule</p></div>
+                <p class="nav-p">|</p>
+                <div class="nav-div"><p v-on:click="pageChange('photos')" class="nav-p">Photos</p></div>
+                <p class="nav-p">|</p>
+                <div class="nav-div"><p v-on:click="pageChange('destination')" class="nav-p">Destination</p></div>
+                <p class="nav-p">|</p>
+                <div class="nav-div"><p v-on:click="pageChange('registry')"  class="nav-p">Registry</p></div>
+                <p class="nav-p">|</p>
+                <div class="nav-div"><p v-on:click="pageChange('accomodations')"  class="nav-p">Accomodations</p></div>
+              </div>
+              <div id="hamburger-div" class="disappear">
+                <img v-on:click="dropItDown()" src="./assets/hamburger_menu.png" style="width: 2rem; height: 90%; margin: 0 3px;"/>
+              </div>
+            </nav>`,
 });
+
